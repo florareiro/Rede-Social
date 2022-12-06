4d1ea26c345b052d126e8f4c7e9ed399c0032fc2
florareiro <ceciliaflora@outlook.com.br>
2022-12-06 11:51:46 -0300

    update serverlUri

--- frontend/src/serverUri.js
+++ frontend/src/serverUri.js
@@ -1,10 +1,3 @@
-const isDevMode =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:5000"
-    : "https://rede-social-backend-dd11t3xco-florareiro.vercel.app/";
-
-const SERVER_URI = isDevMode
-  ? "http://localhost:3000"
-  : ["https://rede-social-frontend-aprq4sdv2-florareiro.vercel.app/"];
-
-module.exports = { SERVER_URI };
+export default process.env.NODE_ENV === "development"
+  ? "http://localhost:5000"
+  : "https://rede-social-backend-dd11t3xco-florareiro.vercel.app/";
