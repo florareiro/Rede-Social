9003f2e1f6dba6028ddc3f3a5a331ab7173d3e14
florareiro <ceciliaflora@outlook.com.br>
2022-12-06 10:42:09 -0300

    update url

--- backend/URI.js
+++ backend/URI.js
@@ -2,6 +2,6 @@ const isDevelopmentMode = process.env.MODE === "DEV";
 
 const clientURL = isDevelopmentMode
   ? "http://localhost:3000"
-  : ["https://rede-social-dz3q-5qgoctg10-florareiro.vercel.app"];
+  : ["rede-social-kkvwiotnu-florareiro.vercel.app"];
 
 module.exports = { clientURL };
--- frontend/src/serverUri.js
+++ frontend/src/serverUri.js
@@ -1,3 +1,3 @@
 export default process.env.NODE_ENV === "development"
   ? "http://localhost:5000"
-  : "https://rede-social-dz3q-5qgoctg10-florareiro.vercel.app";
+  : "https://rede-social-backend-ae1lj9dpi-florareiro.vercel.app/";
