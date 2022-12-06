2c7c65fd5d84337533f80b2ac259477132144d2c
florareiro <ceciliaflora@outlook.com.br>
2022-12-06 11:25:40 -0300

    add .env

--- frontend/src/serverUri.js
+++ frontend/src/serverUri.js
@@ -1,3 +1,6 @@
-export default process.env.NODE_ENV === "development"
+const isDevMode = process.env.NODE_ENV === "development";
+
+const SERVER_URI = isDevMode
   ? "http://localhost:5000"
-  : "https://rede-social-backend-lp6am0bgj-florareiro.vercel.app/";
+  : ["https://rede-social-backend-lp6am0bgj-florareiro.vercel.app/"];
+module.exports = { SERVER_URI };
