83138a26d8da67b39422ce9c397b7fcfbcc5e5f6
florareiro <ceciliaflora@outlook.com.br>
2022-12-06 10:09:30 -0300

    update vercel

--- backend/URI.js
+++ backend/URI.js
@@ -1,5 +1,7 @@
 const isDevelopmentMode = process.env.MODE === "DEV";
 
-const clientURL = isDevelopmentMode ? "http://localhost:3000" : [];
+const clientURL = isDevelopmentMode
+  ? "http://localhost:3000"
+  : ["https://rede-social-hazel.vercel.app/"];
 
 module.exports = { clientURL };
