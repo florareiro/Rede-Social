56fb85a7d666e407dc899e7c9339062ea9fcda8a
florareiro <ceciliaflora@outlook.com.br>
2022-12-07 11:49:15 -0300

    update create post

--- frontend/src/components/CreatePost/CreatePost.jsx
+++ frontend/src/components/CreatePost/CreatePost.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { sendIcon, fileIcon } from "../../assets";
+import { sendIcon } from "../../assets";
 import { useDispatch } from "react-redux";
 import { addPost, updatePost } from "../../features/postSlice";
 import useFetch from "../../hooks/useFetch";
-import Compress from "compress.js";
-import { closeIcon } from "../../assets/index";
 import "./createpost.css";
 
-const initialForm = { image: null, preview: null, caption: "" };
+const initialForm = { caption: "" };
 
 const CreatePost = ({ post, id, close }) => {
   // local states
@@ -15,45 +13,17 @@ const CreatePost = ({ post, id, close }) => {
 
   useEffect(() => {
     if (post && post._id) {
-      setForm({ image: null, preview: post.image?.src, caption: post.caption });
+      setForm({ caption: post.caption });
     }
   }, [post]);
 
   const dispatch = useDispatch();
   const customFetch = useFetch();
-  const compress = new Compress();
-
-  const compressImage = async (files) => {
-    const options = {
-      size: 1,
-      quality: 0.75,
-      maxWidth: 1920,
-      maxHeight: 1920,
-      resize: true,
-      rotate: false,
-    };
-    const data = await compress.compress(files, options);
-    return data;
-  };
-
-  const loadImage = async (e) => {
-    const input = e.target;
-    if (!input) return;
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      setForm((form) => ({ ...form, preview: e.target.result }));
-    };
-    input.files[0] && reader.readAsDataURL(input.files[0]);
-    const files = [...input.files];
-    const data = await compressImage(files);
-    const image = Compress.convertBase64ToFile(data[0]?.data, data[0]?.ext);
-    setForm((form) => ({ ...form, image }));
-  };
 
   const submitHandler = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("image", form.image);
+
     formData.append("caption", form.caption.trim());
     if (post?._id) {
       dispatch(updatePost({ customFetch, id: post._id, formData }));
@@ -68,33 +38,11 @@ const CreatePost = ({ post, id, close }) => {
     <article className="createpost gradient-border">
       <form onSubmit={submitHandler}>
         <textarea
-          placeholder="O que está acontecendo?"
+          placeholder="Compartilhe um pensamento..."
           value={form.caption}
           onChange={(e) => setForm({ ...form, caption: e.target.value })}
         />
-        {form.preview && (
-          <div className="uploaded-image">
-            <img src={form.preview} alt="uploaded file" />
-            <div
-              className="close-icon"
-              onClick={() => setForm({ ...form, image: null, preview: null })}
-            >
-              <img src={closeIcon} alt="remove" />
-            </div>
-          </div>
-        )}
         <div className="btns">
-          <label htmlFor={id || "imagem"} aria-label="selecionar arquivo">
-            <div>
-              <img src={fileIcon} alt="select file" />
-            </div>
-          </label>
-          <input
-            type="file"
-            id={id || "imagem"}
-            accept="image/png, image/jpeg"
-            onChange={loadImage}
-          />
           <button type="submit" aria-label="enviar">
             <img src={sendIcon} alt="send" />
           </button>
